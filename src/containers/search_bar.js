862e95349b3b6127ebe4fb2db3238bce201a73bb
Di Wang <jackiewang5566@hotmail.com>
2017-09-30 14:52:23 -0400

    add onInputChange to input and bind this to avoid this undefined issue

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+        // the instance of SearchBar component has a function onInputChange bind the this to onInputChange
+        // and replace the old onInputChange function to avoid the this issue when using it as a callback
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        
+        this.setState({ term: event.target.value });
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input 
+                    placeholder="Get a five-day forcast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
