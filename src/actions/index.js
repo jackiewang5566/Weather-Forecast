6c1cecc22ecda634bacf58b46f52ebf80a91c447
Di Wang <jackiewang5566@hotmail.com>
2017-10-03 13:24:40 -0400

    change weather endpoint to fix cors error and add reducer_weather reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const API_KEY = '51ff5843fb2943064f143d902994792f';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+    console.log('Request:', request);
 
     return {
         type: FETCH_WEATHER,
